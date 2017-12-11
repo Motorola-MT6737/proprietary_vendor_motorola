2f056fde3d2fecc65524557d743b5a8a3f811162
Carlos Arriaga <carlosarriagacm@gmail.com>
2017-12-10 20:52:14 -0600

    woods: fix patch blobs

--- woods/Android.mk
+++ woods/Android.mk
@@ -19,7 +19,7 @@ LOCAL_PATH := $(call my-dir)
 ifneq ($(filter woods, $(TARGET_DEVICE)),)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libaudiocustparam
-LOCAL_SRC_FILES_32 := vendor/lib/libaudiocustparam.so
+LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libaudiocustparam.so
 LOCAL_MULTILIB := 32
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
@@ -30,7 +30,7 @@ LOCAL_MODULE := libmtkcam_fwkutils
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MULTILIB := 32
-LOCAL_SRC_FILES_32 := vendor/lib/libmtkcam_fwkutils.so
+LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libmtkcam_fwkutils.so
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -38,7 +38,7 @@ LOCAL_MODULE := libdpframework
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX = .so
 LOCAL_MULTILIB := 32
-LOCAL_SRC_FILES_32 := vendor/lib/libdpframework.so
+LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libdpframework.so
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -48,7 +48,7 @@ LOCAL_MODULE_SUFFIX = .a
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_MULTILIB := 32
 LOCAL_SHARED_LIBRARIES_32 := libdpframework
-LOCAL_SRC_FILES_32 := libstagefright_color_conversion/libstagefright_color_conversion_32.a
+LOCAL_SRC_FILES_32 := proprietary/vendor/libstagefright_color_conversion/libstagefright_color_conversion_32.a
 include $(BUILD_PREBUILT)
 
 endif

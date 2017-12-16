8bfbbfa5286c82235d0427fa97efa861e0046ea2
Carlos Arriaga <carlosarriagacm@gmail.com>
2017-12-16 15:28:50 -0600

    revert fix

--- woods/Android.mk
+++ woods/Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter woods, $(TARGET_DEVICE)),)
+ifneq ($(filter nicklaus, $(TARGET_DEVICE)),)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libaudiocustparam
 LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libaudiocustparam.so
@@ -24,37 +24,31 @@ LOCAL_MULTILIB := 32
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
 include $(BUILD_PREBUILT)
- 
+
 include $(CLEAR_VARS)
-LOCAL_MODULE = libmtkcam_fwkutils
-LOCAL_MODULE_CLASS = SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX = .so
-LOCAL_MULTILIB = 32
-LOCAL_SRC_FILES_32 = proprietary/vendor/lib/libmtkcam_fwkutils.so
-include $(BUILD_PREBUILT)
- 
-include $(CLEAR_VARS)
-LOCAL_MODULE = libdpframework
-LOCAL_MODULE_CLASS = SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX = .so
-LOCAL_MULTILIB = 32
-LOCAL_SRC_FILES_32 = proprietary/vendor/lib/libdpframework.so
+LOCAL_MODULE := libmtkcam_fwkutils
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MULTILIB := 32
+LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libmtkcam_fwkutils.so
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE = libion_mtk
-LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE := libdpframework
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX = .so
-LOCAL_MULTILIB = 32
-LOCAL_SRC_FILES_32 = proprietary/vendor/lib/libion_mtk.so
+LOCAL_MULTILIB := 32
+LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libdpframework.so
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE = libged
-LOCAL_MODULE_CLASS = SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX = .so
-LOCAL_MULTILIB = 32
-LOCAL_SRC_FILES_32 = proprietary/vendor/lib/libged.so
+LOCAL_MODULE := libstagefright_color_conversion
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+LOCAL_MODULE_SUFFIX = .a
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_MULTILIB := 32
+LOCAL_SHARED_LIBRARIES_32 := libdpframework
+LOCAL_SRC_FILES_32 := proprietary/libstagefright_color_conversion/libstagefright_color_conversion_32.a
 include $(BUILD_PREBUILT)
 
 endif
